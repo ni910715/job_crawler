6086fad5c84f5ed080b51fb26da4a6a2470f98cd
倪大為 <nidawei@nidaweideMacBook-Pro.local>
2024-05-08 17:18:51 +0800

    dataframe and excel

--- main.py
+++ main.py
@@ -2,21 +2,43 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import time
+import pandas as pd
 
 def crawl(page):
     count = 0
     while count < page:
+        all_jobs = []
+
         jobs = driver.find_elements(By.TAG_NAME, "article")
         for job in jobs:
             try:
-                title = job.find_element(By.TAG_NAME, "a")
-                print(title.text)
+                title = job.find_elements(By.TAG_NAME, "a")[0].text
+                # print(title.text)
+
+                company = job.find_elements(By.TAG_NAME, "a")[1].text
+                # print(company.text)
+
+                locate = job.find_element(By.CLASS_NAME, "job-list-intro").find_elements(By.TAG_NAME, "li")[0].text
+                # print(locate.text)
+
+                experience = job.find_element(By.CLASS_NAME, "job-list-intro").find_elements(By.TAG_NAME, "li")[1].text
+                # print(experience.text)
+
+                degree = job.find_element(By.CLASS_NAME, "job-list-intro").find_elements(By.TAG_NAME, "li")[2].text
+                # print(degree.text)
+
+                # print("-" * 30)
+
+                all_jobs.append({"職缺名稱":title, "公司名稱":company, "位置":locate, "經驗":experience, "學位":degree})
+
             except Exception:
                 break
         count += 1
         next_btn = driver.find_element(By.CLASS_NAME, "js-next-page")
         next_btn.click()
         time.sleep(3)
+    
+    return pd.DataFrame(all_jobs)
 
 
 
@@ -53,14 +75,9 @@ search = driver.find_element(By.CLASS_NAME, "btn-secondary")
 search.click()
 time.sleep(3)
 
-crawl(2)
+data = crawl(2)
+print(data)
 
+data.to_excel("job.xlsx")
 
-
-
-
-
-
-
-time.sleep(100)
 driver.close()
