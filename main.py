204c340ca575a7ca0cd793be4ad323c7ddc358ed
倪大為 <nidawei@nidaweideMacBook-Pro.local>
2024-05-09 17:23:36 +0800

    fix crawl mistake

--- main.py
+++ main.py
@@ -6,41 +6,37 @@ import pandas as pd
 
 def crawl(page):
     count = 0
+    all_jobs = []
     while count < page:
-        all_jobs = []
 
         jobs = driver.find_elements(By.TAG_NAME, "article")
         for job in jobs:
             try:
                 title = job.find_elements(By.TAG_NAME, "a")[0].text
-                # print(title.text)
 
                 company = job.find_elements(By.TAG_NAME, "a")[1].text
-                # print(company.text)
 
                 locate = job.find_element(By.CLASS_NAME, "job-list-intro").find_elements(By.TAG_NAME, "li")[0].text
-                # print(locate.text)
 
                 experience = job.find_element(By.CLASS_NAME, "job-list-intro").find_elements(By.TAG_NAME, "li")[1].text
-                # print(experience.text)
 
                 degree = job.find_element(By.CLASS_NAME, "job-list-intro").find_elements(By.TAG_NAME, "li")[2].text
-                # print(degree.text)
-
-                # print("-" * 30)
 
                 all_jobs.append({"職缺名稱":title, "公司名稱":company, "位置":locate, "經驗":experience, "學位":degree})
 
-            except Exception:
-                break
+            except Exception as e:
+                print(e)
+                # time.sleep(1000)
+        
+        print(f'目前爬蟲{count+1}頁')
+
         count += 1
-        next_btn = driver.find_element(By.CLASS_NAME, "js-next-page")
-        next_btn.click()
-        time.sleep(3)
-    
+        if count < page:
+            next_btn = driver.find_element(By.CLASS_NAME, "js-next-page")
+            next_btn.click()
+            time.sleep(5)
+
     return pd.DataFrame(all_jobs)
-
-
 
 options = Options()
 options.executable_chrome_path = "/Users/nidawei/git-repos/104-job-crawler/chromedriver"
@@ -62,7 +58,7 @@ area_confirm_btn.click()
 time.sleep(1)
 category = driver.find_element(By.XPATH, "//button/span[contains(text(), '職務類別')]")
 category.click()
-time.sleep(1)
+time.sleep(2)
 information = driver.find_element(By.LINK_TEXT, "資訊軟體系統類")
 information.click()
 software = driver.find_element(By.CSS_SELECTOR, "input[value='2007001000']")
@@ -73,11 +69,15 @@ information_confirm.click()
 time.sleep(1)
 search = driver.find_element(By.CLASS_NAME, "btn-secondary")
 search.click()
-time.sleep(3)
+time.sleep(5)
 
-data = crawl(2)
+data = crawl(3)
 print(data)
 
-data.to_excel("job.xlsx")
+condition1 = (data["經驗"] == "經歷不拘") & (data["學位"] != "碩士")
+new_data = data[condition1]
+print(new_data)
 
+data.to_excel("job.xlsx")
+time.sleep(1000)
 driver.close()
