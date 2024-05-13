ca5f9c14e1e443d7dda576edf30b43ebd7a39675
倪大為 <nidawei@nidaweideMacBook-Pro.local>
2024-05-14 00:18:07 +0800

    switch window and get first skill

--- main.py
+++ main.py
@@ -17,24 +17,43 @@ def crawl(page):
 
         for job in jobs:
             try:
-                title = job.find_elements(By.TAG_NAME, "a")[0].text
-                # print(title)
+                # window
+
+                title = job.find_elements(By.TAG_NAME, "a")[0]
+
+                job_name = title.text
 
                 company = job.find_elements(By.TAG_NAME, "a")[1].text
-                # print(company)
 
                 locate = job.find_element(By.CLASS_NAME, "job-list-intro").find_elements(By.TAG_NAME, "li")[0].text
-                # print(locate)
 
                 experience = job.find_element(By.CLASS_NAME, "job-list-intro").find_elements(By.TAG_NAME, "li")[1].text
-                # print(experience)
 
                 degree = job.find_element(By.CLASS_NAME, "job-list-intro").find_elements(By.TAG_NAME, "li")[2].text
-                # print(degree)
 
-                all_jobs.append({"職缺名稱":title, "公司名稱":company, "位置":locate, "經驗":experience, "學位":degree})
-                # print("append to list")
-                # print("="*30)
+                # all_windows = driver.window_handles
+                # new_window = [window for window in all_windows if window != main_window][0]
+                main_window = driver.current_window_handle
+                title.click()
+                time.sleep(2)
+
+                new_window = driver.window_handles[1]
+
+                driver._switch_to.window(new_window)
+                
+                try:
+                    skill = driver.find_element(By.CSS_SELECTOR, "a[class='tools text-gray-deep-dark d-inline-block']").text
+                except Exception as e:
+                    table = driver.find_elements(By.CSS_SELECTOR, "div[class='job-requirement-table row'] div[class='list-row row mb-2']")[4]
+                    skill = table.find_element(By.CSS_SELECTOR, "div[class='t3 mb-0']").text
+                print(skill)
+
+                time.sleep(3)
+                driver.close()
+                driver.switch_to.window(main_window)
+
+                all_jobs.append({"職缺名稱":job_name, "公司名稱":company, "位置":locate, "經驗":experience, "學位":degree})
+
             except Exception as e:
                 print(e)
                 # print(len(jobs))
@@ -48,24 +67,26 @@ def crawl(page):
 
     return pd.DataFrame(all_jobs)
 
+# driver path
 options = Options()
 options.executable_chrome_path = "/Users/nidawei/git-repos/104-job-crawler/chromedriver"
 
+# url path
 driver = webdriver.Chrome(options=options)
 driver.get("https://www.104.com.tw/jobs/main/")
-time.sleep(1)
+time.sleep(2)
 
 area = driver.find_element(By.XPATH,"//button/span[contains(text(), '地區')]")
 area.click()
-time.sleep(1)
+time.sleep(2)
 taipei = driver.find_element(By.CSS_SELECTOR, "input[value='6001001000']")
 taipei.click()
 new_taipei = driver.find_element(By.CSS_SELECTOR, "input[value='6001002000']")
 new_taipei.click()
-time.sleep(1)
+time.sleep(2)
 area_confirm_btn = driver.find_element(By.CLASS_NAME, "category-picker-btn-primary")
 area_confirm_btn.click()
-time.sleep(1)
+time.sleep(2)
 category = driver.find_element(By.XPATH, "//button/span[contains(text(), '職務類別')]")
 category.click()
 time.sleep(2)
@@ -73,16 +94,16 @@ information = driver.find_element(By.LINK_TEXT, "資訊軟體系統類")
 information.click()
 software = driver.find_element(By.CSS_SELECTOR, "input[value='2007001000']")
 software.click()
-time.sleep(1)
+time.sleep(2)
 information_confirm = driver.find_element(By.CLASS_NAME, "category-picker-btn-primary")
 information_confirm.click()
-time.sleep(1)
+time.sleep(2)
 search = driver.find_element(By.CLASS_NAME, "btn-secondary")
 search.click()
 time.sleep(5)
 
 # job crawler
-data = crawl(10)
+data = crawl(1)
 
 # filter
 condition1 = (data["經驗"] == "經歷不拘") & (data["學位"] != "碩士")
@@ -92,4 +113,5 @@ new_data = data[condition1]
 data.to_excel("all_job.xlsx")
 new_data.to_excel("filter_job.xlsx")
 
+# time.sleep(1000)
 driver.close()
