433b939e5f72cbe9842b07626e77af0b9d7fbe51
倪大為 <nidawei@nidaweideMacBook-Pro.local>
2024-05-16 13:11:23 +0800

    get link

--- main.py
+++ main.py
@@ -1,8 +1,41 @@
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import time
 import pandas as pd
+from tabulate import tabulate
+
+def crawl_detail(title):
+    # window
+    main_window = driver.current_window_handle
+    title.click()
+    time.sleep(2)
+
+    new_window = driver.window_handles[1]
+
+    driver._switch_to.window(new_window)
+    skill_list = []
+    try:
+        skills = driver.find_elements(By.CSS_SELECTOR, "a[class='tools text-gray-deep-dark d-inline-block']")
+        if skills:
+            for skill in skills:
+                skill_list.append(skill.text)
+        else:
+            table = driver.find_elements(By.CSS_SELECTOR, "div[class='job-requirement-table row'] div[class='list-row row mb-2']")[4]
+            skill = table.find_element(By.CSS_SELECTOR, "div[class='t3 mb-0']")
+            skill_list.append(skill.text)
+        
+    except Exception as e:
+        print(f"detail page:{e}")
+    
+    time.sleep(3)
+    driver.close()
+    driver.switch_to.window(main_window)
+
+    skill_str = ', '.join(skill_list)
+
+    return skill_str
 
 def crawl(page):
     count = 0
@@ -19,6 +52,7 @@ def crawl(page):
             try:
 
                 title = job.find_elements(By.TAG_NAME, "a")[0]
+                link = title.get_attribute('href')
 
                 job_name = title.text
 
@@ -30,33 +64,9 @@ def crawl(page):
 
                 degree = job.find_element(By.CLASS_NAME, "job-list-intro").find_elements(By.TAG_NAME, "li")[2].text
 
-                # window
-                main_window = driver.current_window_handle
-                title.click()
-                time.sleep(2)
+                skill = crawl_detail(title)
 
-                new_window = driver.window_handles[1]
-
-                driver._switch_to.window(new_window)
-                skill_list = []
-                try:
-                    skills = driver.find_elements(By.CSS_SELECTOR, "a[class='tools text-gray-deep-dark d-inline-block']")
-                    if skills:
-                        for skill in skills:
-                            skill_list.append(skill.text)
-                    else:
-                        table = driver.find_elements(By.CSS_SELECTOR, "div[class='job-requirement-table row'] div[class='list-row row mb-2']")[4]
-                        skill = table.find_element(By.CSS_SELECTOR, "div[class='t3 mb-0']").text
-                        skill_list.append(skill)
-
-                except Exception as e:
-                    print(f"detail page:{e}")
-                
-                time.sleep(3)
-                driver.close()
-                driver.switch_to.window(main_window)
-
-                all_jobs.append({"職缺名稱":job_name, "公司名稱":company, "位置":locate, "經驗":experience, "學位":degree, "技能":skill_list})
+                all_jobs.append({"職缺名稱":job_name, "公司名稱":company, "位置":locate, "經驗":experience, "學位":degree, "技能":skill, "連結":link})
 
             except Exception as e:
                 print(f"main page:{e}")
@@ -108,6 +118,7 @@ time.sleep(5)
 
 # job crawler
 data = crawl(1)
+# print(tabulate(data, headers='keys', tablefmt='psql'))
 
 # filter
 condition1 = (data["經驗"] == "經歷不拘") & (data["學位"] != "碩士")
